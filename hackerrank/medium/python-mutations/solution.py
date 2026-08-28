7f2a5c80503465b25444e0760304777fa65930b0
sumit midya <sumitmidya09@gmail.com>
2026-08-28 21:05:25 +0530

    ✅ Mutations | Medium | Python | Runtime: N/A | Memory: N/A | 2026-08-28

---
+++ hackerrank/medium/python-mutations/solution.py
@@ -0,0 +1,23 @@
+
+
+def mutate_string(string, position, character):
+    a=list(s)
+    b=[]
+    for j in range(len(s)):
+        if j==int(i):
+            b.append(str(c))
+        elif j<int(i):
+            b.append(s[j])
+        else:
+            k=1
+            while k>0:
+                f=j
+                b.append(s[f])
+                break
+    
+
+
+
+    return ''.join(b)
+
+
