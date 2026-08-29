4c7f1f918fb873a8d0e6f6cac281f6d5ff3d88d6
sumit midya <sumitmidya09@gmail.com>
2026-08-29 09:27:17 +0530

    ✅ Alphabet Rangoli | Easy | Python | Runtime: N/A | Memory: N/A | 2026-08-29

--- hackerrank/easy/alphabet-rangoli/solution.py
+++ hackerrank/easy/alphabet-rangoli/solution.py
@@ -1,11 +1,18 @@
-def print_formatted(number):
-    
+def print_rangoli(size):
     # your code goes here
-    width = len(f"{number:b}")
-    for i in range(1, number + 1):
-        print(
-        f"{i:>{width}} "
-        f"{i:>{width}o} "
-        f"{i:>{width}X} "
-        f"{i:>{width}b}"
-    )
+    rows = size*2-1
+    columns = (n*2-1)+(n*2-2)
+    k = (rows // 2)+1
+    j = k - 1
+
+    for i in range(1,rows+1):
+        if(k >= i):
+            l1 = [chr(x) for x in range((96+size),(96+size-i),-1)]
+            l2 = [chr(x) for x in range((96+size-i+2),(96+size+1))]       
+        else:
+            l1 = [chr(x) for x in range((96+size),(96+size-j),-1)]
+            l2 = [chr(x) for x in range((96+size-j+2),(96+size+1))]          
+            j -= 1
+        l = l1+l2
+        string = '-'.join(l)
+        print(string.center(columns,'-'))
